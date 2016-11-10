1ab8994aca49a9d6aebd81005d3fae3235948932
Vytenis <vytenis.butkevicius@gmail.com>
2016-11-10 20:50:37 +0200

    Change public path for built files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/static/react/',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
