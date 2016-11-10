90576dc246861c0409c7823ae6b34fa4c7a882ab
Vytenis <vytenis.butkevicius@gmail.com>
2016-11-10 22:01:59 +0200

    Fix path name

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -23,7 +23,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/static/react/',
+    publicPath: '/public/react/',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
